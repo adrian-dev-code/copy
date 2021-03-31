50caef37e8b3eb701ae91b1e240e93c8604a9451
adrian-dev-code <52628185+adrian-dev-code@users.noreply.github.com>
2021-03-31 17:38:53 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,16 +1,10 @@
 #include <xc.h> 
 __PROG_CONFIG(1,0x3FE4); 
 
-void int_uC(void){
-   TRISB = 0x00;
-   TRISD = 0x00;
-   ANSELB=0;
-   ANSELD=0;
-}
 unsigned char a;
 unsigned long y;
+void int_uC(void);
 void main(void){
-   PORTD=0;
     int_uC();
    while (1)
    {
@@ -20,12 +14,12 @@ void main(void){
 	 for(y=0; y<1000; y++); 
       }
       PORTB=a;
-       for(y=0; y<1000; y++); 
+       for(y=0; y<1000; y++);
       PORTB=0;
       
        
        
-       for(a=0x04;a<0x80;a<<=1)
+       for(a=0x04;a<0x40;a<<=1)
       {
 	 if(a==0x10) a<<=1;
 	  PORTD=a;
@@ -43,10 +37,18 @@ void main(void){
 	PORTD=0;
      
 	 
-	 for(a=0x80;a>0x10;a>>=1)
+	 for(a=0x80;a>0x20;a>>=1)
 	{
 	 PORTB=a;
 	   for(y=0; y<1000; y++); 
 	} 
      }
   }
+  void int_uC(void){
+   PORTD=0;
+   PORTB=0;
+   TRISB = 0x00;
+   TRISD = 0x00;
+   ANSELB=0x00;
+   ANSELD=0x00;
+}
